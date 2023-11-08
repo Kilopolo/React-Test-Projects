ea37c2ed8321690760f309a394bb71cbbd3a3129
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-11-08 10:01:29 +0100

    Update App.js

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -3,19 +3,31 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import styles from "./AppStyles";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
   function goalInputHandler(enteredText) {
-    console.log(enteredText);
+    setEnteredGoalText(enteredText);
   }
-  function addGoalHandler() {}
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput onChangeText={goalInputHandler} style={styles.textInput}>Text here...</TextInput>
+        <TextInput onChangeText={goalInputHandler} style={styles.textInput}>
+          ...
+        </TextInput>
         <Button onPress={addGoalHandler} title="Add"></Button>
       </View>
       <View style={styles.listContainer}>
-        <Text>List of goals</Text>
+        {courseGoals.map((goal, index) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
