4e8598538824dad7f89c4ea896d39c685c29f61e
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-12-03 19:52:32 +0100

    pruebas

---
+++ my-first-react-app/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>LogIn Page</h2>
+      {/* Aquí puedes añadir tu formulario de inicio de sesión */}
+    </div>
+  );
+}
+
+export default Login;
---
+++ my-first-react-app/src/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h2>SignUp Page</h2>
+      {/* Aquí puedes añadir tu formulario de registro */}
+    </div>
+  );
+}
+
+export default SignUp;
