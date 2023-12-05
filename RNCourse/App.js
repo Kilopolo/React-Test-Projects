af70cab45c4e33b18ed68af5d25162c91d6997cc
Kilopolo <pablo23dr@gmail.com>
2023-12-05 16:32:20 +0100

    d

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -17,18 +17,33 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput onChangeText={goalInputHandler} style={styles.textInput}>
-          ...
-        </TextInput>
-        <Button onPress={addGoalHandler} title="Add"></Button>
+    // <View style={styles.container}>
+    //   <View style={styles.inputContainer}>
+    //     <TextInput onChangeText={goalInputHandler} style={styles.textInput}>
+    //       ...
+    //     </TextInput>
+    //     <Button onPress={addGoalHandler} title="Add"></Button>
+    //   </View>
+    //   <View style={styles.listContainer}>
+    //     {courseGoals.map((goal, index) => (
+    //       <Text key={goal}>{goal}</Text>
+
+    //     ))}
+    //   </View>
+    // </View>
+    <View style={styles.mainContainer}>
+      <View style={styles.headContainer}>
+
       </View>
-      <View style={styles.listContainer}>
-        {courseGoals.map((goal, index) => (
-          <Text key={goal}>{goal}</Text>
-        ))}
+      <View style={styles.enemyContainer}>
+        <View style={styles.enemyContainerLeft}></View>
+        <View style={styles.enemyContainerRight}></View>
       </View>
+      <View style={styles.playerContainer}>
+        <View style={styles.playerContainerLeft}></View>
+        <View style={styles.playerContainerRight}></View>
+      </View>
+      <View style={styles.footerContainer}></View>
     </View>
   );
 }
--- RNCourse/AppStyles.js
+++ RNCourse/AppStyles.js
@@ -30,4 +30,59 @@ export default StyleSheet.create({
     justifyContent:'space-between',
 
   },
+  mainContainer:{
+    flex: 1,
+    flexDirection: "column",
+
+  },
+  headContainer:{
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#00000',
+    marginTop: 20,
+  },
+  enemyContainer:{
+    flex: 3,
+    borderWidth: 1,
+    flexDirection: "row",
+    borderColor: '#00000',
+    margin: 10,
+  },
+  playerContainer:{
+    flex: 3,
+    borderWidth: 1,
+    flexDirection: "row",
+    borderColor: '#00000',
+    margin: 10,
+  },
+  enemyContainerLeft:{
+    flex: 3,
+    borderWidth: 1,
+    borderColor: '#00000',
+    margin: 10,
+  },
+  playerContainerLeft:{
+    flex: 3,
+    borderWidth: 1,
+    borderColor: '#00000',
+    margin: 10,
+  },
+  enemyContainerRight:{
+    flex: 3,
+    borderWidth: 1,
+    borderColor: '#00000',
+    margin: 10,
+  },
+  playerContainerRight:{
+    flex: 3,
+    borderWidth: 1,
+    borderColor: '#00000',
+    margin: 10,
+  },
+  footerContainer:{
+    flex: 2,
+    borderWidth: 1,
+    borderColor: '#00000',
+    marginBottom: 20,
+  },
 });
